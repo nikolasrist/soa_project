9439908a1dd5fbf88224ef7a436698b817e3d167
Nikolas Rist <nikolas.rist@leanix.net>
2019-09-06 09:57:32 +0200

    Provide salesman form and print in next step

--- camunda_client/src/main/java/ServiceClient.java
+++ camunda_client/src/main/java/ServiceClient.java
@@ -4,6 +4,7 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.Set;
 import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.delegate.JavaDelegate;
 import org.slf4j.Logger;
@@ -16,41 +17,46 @@ public class ServiceClient implements JavaDelegate {
 
         LOG.info("Send request to Data collector.");
 
-        HttpURLConnection connection = null;
-        String targetURL = "https://data-collector.ironmanserver.de"; // TODO: 2019-09-02 add correct url
-        try {
-            //Create connection
-            URL url = new URL(targetURL);
-            connection = (HttpURLConnection) url.openConnection();
-            connection.setRequestMethod("GET");
-            connection.setRequestProperty("Content-Type",
-                "application/x-www-form-urlencoded");
-            connection.setRequestProperty("Content-Language", "en-US");
-            connection.setUseCaches(false);
-            connection.setDoOutput(true);
+        Set<String> variableNames = execution.getVariableNames();
+        LOG.info("Variables: {}", variableNames);
+        String salesman = (String) execution.getVariable("NameField");
+        LOG.info("Salesman chosen: {}", salesman);
 
-            //Send request
-            DataOutputStream wr = new DataOutputStream (
-                connection.getOutputStream());
-            wr.close();
-
-            //Get Response
-            InputStream is = connection.getInputStream();
-            BufferedReader rd = new BufferedReader(new InputStreamReader(is));
-            StringBuilder response = new StringBuilder(); // or StringBuffer if Java version 5+
-            String line;
-            while ((line = rd.readLine()) != null) {
-                response.append(line);
-                response.append('\r');
-            }
-            rd.close();
-            System.out.println(response.toString());
-            execution.setVariable("listOfSalsemen", response.toString());
-        } finally {
-            if (connection != null) {
-                connection.disconnect();
-            }
-        }
+//        HttpURLConnection connection = null;
+//        String targetURL = "https://data-collector.ironmanserver.de"; // TODO: 2019-09-02 add correct url
+//        try {
+//            //Create connection
+//            URL url = new URL(targetURL);
+//            connection = (HttpURLConnection) url.openConnection();
+//            connection.setRequestMethod("GET");
+//            connection.setRequestProperty("Content-Type",
+//                "application/x-www-form-urlencoded");
+//            connection.setRequestProperty("Content-Language", "en-US");
+//            connection.setUseCaches(false);
+//            connection.setDoOutput(true);
+//
+//            //Send request
+//            DataOutputStream wr = new DataOutputStream (
+//                connection.getOutputStream());
+//            wr.close();
+//
+//            //Get Response
+//            InputStream is = connection.getInputStream();
+//            BufferedReader rd = new BufferedReader(new InputStreamReader(is));
+//            StringBuilder response = new StringBuilder(); // or StringBuffer if Java version 5+
+//            String line;
+//            while ((line = rd.readLine()) != null) {
+//                response.append(line);
+//                response.append('\r');
+//            }
+//            rd.close();
+//            System.out.println(response.toString());
+//            execution.setVariable("listOfSalsemen", response.toString());
+//        } finally {
+//            if (connection != null) {
+//                connection.disconnect();
+//            }
+//        }
 
     }
 }
