6feba6b6b84cec7af753be035786b16816589134
Kai Bepperling <kai.bepperling@gmail.com>
2019-09-15 15:18:33 +0200

    remove ignoreproperties

--- camunda_client/src/main/java/ClientInfoDTO.java
+++ camunda_client/src/main/java/ClientInfoDTO.java
@@ -1,9 +1,13 @@
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import java.util.List;
 
 //@JsonIgnoreProperties(ignoreUnknown = true)
 public class ClientInfoDTO {
+    @JsonProperty("salesmanName")
     private String salesmanName;
+    @JsonProperty("salesInfos")
     private List<SalesInfoDTO> salesInfos;
 
     public String getClientName() {
